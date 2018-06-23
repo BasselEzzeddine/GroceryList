5c62cfcbdd279ddeae0711f1de27b252f442edf3
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-06-23 14:01:39 +0200

    Created Xcode project with empty components

---
+++ GroceryList/GroceryList/Scenes/Basket/BasketViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BasketViewController.swift
+//  GroceryList
+//
+//  Created by Bassel Ezzeddine on 23/06/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import UIKit
+
+class BasketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
