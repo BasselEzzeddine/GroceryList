332c23575602c26c4ab90946674917c2005188b4
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-06-24 15:13:50 +0200

    Created MockServer with getCurrencyRates mocks

---
+++ GroceryList/GroceryListTests/MockServer/MockServer.swift
@@ -0,0 +1,67 @@
+//
+//  MockServer.swift
+//  GroceryListTests
+//
+//  Created by Bassel Ezzeddine on 24/06/2018.
+//  Copyright © 2018 Bassel Ezzeddine. All rights reserved.
+//
+
+import Foundation
+import GCDWebServer
+
+class MockServer {
+    
+    // MARK: - Properties
+    let webServer = GCDWebServer()
+    
+    // MARK: - Methods
+    func start() {
+        do {
+            try webServer.start(options: [
+                GCDWebServerOption_AutomaticallySuspendInBackground: false,
+                GCDWebServerOption_Port: 8080,
+                GCDWebServerOption_BindToLocalhost: true])
+            print("Started mock server on port 8080")
+        }
+        catch let error {
+            print("Mock server could not start : \(error)")
+        }
+    }
+    
+    func stop() {
+        webServer.stop()
+        reset()
+    }
+    
+    func reset() {
+        webServer.removeAllHandlers()
+    }
+    
+    func getDataFromFile(fileName: String, type: String) throws -> Data {
+        let path = Bundle(for: MockServer.self).path(forResource: fileName, ofType: type)
+        let data = try Data(contentsOf: URL(fileURLWithPath: path!), options: NSData.ReadingOptions.mappedIfSafe)
+        return data
+    }
+    
+    // MARK: - Mocks
+    func respondToGetCurrencyRatesWithSuccess(mockFileName: String = "getCurrencyRates") {
+        URLCache.shared.removeAllCachedResponses()
+        webServer.addHandler(forMethod: "GET", path: "/api/live", request: GCDWebServerRequest.self, processBlock: { request in
+            var data: Data!
+            do {
+                data = try self.getDataFromFile(fileName: mockFileName, type: "json")
+            }
+            catch _ {}
+            return GCDWebServerDataResponse(data: data, contentType: "json")
+        })
+    }
+    
+    func respondToGetCurrencyRatesWithError(statusCode: Int = 500) {
+        URLCache.shared.removeAllCachedResponses()
+        webServer.addHandler(forMethod: "GET", path: "/api/live", request: GCDWebServerRequest.self, processBlock: { request in
+            let errorResponse = GCDWebServerErrorResponse(data: Data(), contentType: "json")
+            errorResponse.statusCode = statusCode
+            return errorResponse
+        })
+    }
+}
